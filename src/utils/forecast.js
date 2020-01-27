a6fdc7473563e5dd669fdfc08fe80b52fdbefa9f
noeljansen <noel.jansen@gmail.com>
2020-01-27 15:04:16 +0800

    Added Min and Max temperatures to data string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,17 +11,17 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/ffbb2be4010ab5a5f24229995e889106/'+encodeURIComponent(latitude) +', ' + encodeURIComponent(longitude)
-    request({url, json: true}, (error, {body}) =>{
-        if (error){
+    const url = 'https://api.darksky.net/forecast/ffbb2be4010ab5a5f24229995e889106/' + encodeURIComponent(latitude) + ', ' + encodeURIComponent(longitude)
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
             callback('Unable to connect to the forecast services!')
-        } else if(body.error){
+        } else if (body.error) {
             callback('Unable to find location.')
         } else {
             const currently = body.currently
-            callback(undefined, body.daily.data[0].summary+ ' It is currently ' + currently.temperature + ' degrees out. There is a ' + currently.precipProbability +'% chance of rain.' )
-        } 
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + currently.temperature + ' degrees out. There is a ' + currently.precipProbability + '% chance of rain. The minimum temperature is ' + body.daily.data[0].temperatureMin + " and the maximum temperature is " + body.daily.data[0].temperatureMax + ".")
+        }
     })
 }
- 
+
 module.exports = forecast
